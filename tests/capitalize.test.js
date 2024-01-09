1c42d218ea3d130f9d08b66194c4592048e43a0e
1romang1 <krglrv@gmail.com>
2024-01-09 12:04:15 +0300

    test node:assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,14 +1,16 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
-
+import { strict as assert } from 'node:assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-console.log('Все тесты пройдены!');
+// console.log('Все тесты пройдены!');
+assert.deepEqual(capitalize(''), '')
+assert.deepEqual(capitalize('hello'), 'Hello')
